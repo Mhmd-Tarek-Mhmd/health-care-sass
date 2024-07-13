d6614da821da6b6692ac75040e1f8cf81e84a2ba
Mhmd <mhmd.tarek197@gmail.com>
2024-07-14 02:34:26 +0300

    ADD: loader component

---
+++ src/App/components/Loader/index.tsx
@@ -0,0 +1,31 @@
+import styles from "./loader.module.css";
+import { useTranslation } from "react-i18next";
+import { Box, Spinner, VisuallyHidden } from "@chakra-ui/react";
+
+interface LoaderProps {
+  h?: number;
+  size?: string;
+  fixed?: boolean;
+  absolute?: boolean;
+  isLoading: boolean;
+}
+
+const Loader = ({ h, size, isLoading, fixed, absolute }: LoaderProps) => {
+  const { t } = useTranslation();
+
+  const classes = fixed ? styles.fixed : absolute ? styles.absolute : "";
+
+  return isLoading ? (
+    <Box
+      sx={{ h }}
+      role="alert"
+      aria-live="assertive"
+      className={`${styles.loader} ${classes}`.trim()}
+    >
+      <VisuallyHidden>{t("loader-label")}</VisuallyHidden>
+      <Spinner size={size || "xl"} />
+    </Box>
+  ) : null;
+};
+
+export default Loader;
--- src/App/components/index.ts
+++ src/App/components/index.ts
@@ -2,6 +2,7 @@ export * from "./ConfirmDialog";
 export * from "./IconButtonWithTooltip";
 
 export { default as Logo } from "./Logo";
+export { default as Loader } from "./Loader";
 export { default as FormInput } from "./FormInput";
 export { default as DataTable } from "./DataTable";
 export { default as ThemeToggler } from "./ThemeToggler";
